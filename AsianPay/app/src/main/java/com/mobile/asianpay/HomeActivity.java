3572b33e71990ce1f74a7e5e1fc2fcb582bda63b
RohilPrajapati <rohilprajapati@gmail.com>
2023-04-05 07:50:06 +0545

    todays class room

--- AsianPay/app/src/main/java/com/mobile/asianpay/HomeActivity.java
+++ AsianPay/app/src/main/java/com/mobile/asianpay/HomeActivity.java
@@ -2,6 +2,7 @@ package com.mobile.asianpay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -13,11 +14,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        ImageView img_icon;
+        getSupportActionBar().hide();
+        ImageView img_icon,btn_profile;
         TextView txt_amt;
+
         txt_amt = findViewById(R.id.txt_amt);
         img_icon = findViewById(R.id.img_icon);
+        btn_profile = findViewById(R.id.btn_profile);
+
 
         img_icon.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +33,14 @@ public class HomeActivity extends AppCompatActivity {
                     txt_amt.setText("Rs. XXXX.XX");
                     showAmt = true;
                 }
+            }
+        });
+
+        btn_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this,ProfileActivity.class);
+                startActivity(intent);
             }
         });
     }
